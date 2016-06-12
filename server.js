a57831fc1fcde8d8a5bb83295703c719392a461c
Scott Conway <conway.sc@husy.neu.edu>
2016-06-11 22:46:44 -0400

    testing socket connection

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var express = require('express'),
 
 // Create an express instance and set a port variable
 var app = express();
-var port = process.env.PORT || 1243;
+// var port = process.env.PORT || 1243;
 
 // Set handlebars as the templating engine
 // app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
@@ -41,9 +41,9 @@ app.get('/page/:page/:skip', routes.page);
 app.use("/", express.static(__dirname + "/public"));
 
 // Fire this bitch up (start our server)
-var server = http.createServer(app).listen(port, function() {
-  console.log('Express server listening on port ' + port);
-});
+// var server = http.createServer(app).listen(port, function() {
+//   console.log('Express server listening on port ' + port);
+// });
 
 // Initialize socket.io
 var io = require('socket.io').listen(server);
